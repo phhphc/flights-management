2eb6f618aad0f22d8a53ef8dcdda7dbcc1c31f52
pham phuoc <phhphc@gmail.com>
2022-06-15 06:34:20 +0700

    fix: remove unuse module

--- customers/views.py
+++ customers/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from matplotlib.axis import Tick
 
 from base_app.models import Flight, FlightTicket, IntermediateAirport, Ticket, Regulations
 from base_app.forms import TicketForm, CustomUserForm
